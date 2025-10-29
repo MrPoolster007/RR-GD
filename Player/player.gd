9c116746e5002ed7e105ef79973895a0c0a30bd9
Nimish <lanjewar.nimish@gmail.com>
2025-10-29 19:49:23 +0530

    Deploying RR till enemy movemnt

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED := 4.5
 var mouse_motion := Vector2.ZERO
 @onready var pivot: Node3D = $Pivot
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_sens = 0.01
 
-var mouse_sens = 0.02
-
+@export var fall_multipler := 2.0
+@export var jump_height := 1.0
 # runs at start of the scene
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -16,11 +16,13 @@ func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		if velocity.y > 0:
+			velocity.y -= gravity * delta
+		else:
+			velocity.y -= gravity * delta * fall_multipler
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
